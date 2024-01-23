7bcf80f99a4dcf4f7ff79b13a45fffd5cf58f740
Andrew Burtsevich <andersbrtsvch@gmail.com>
2024-01-23 13:46:06 +0300

    some improvements

--- core/src/main/java/by/academy/dto/UserFullDataDTO.java
+++ core/src/main/java/by/academy/dto/UserFullDataDTO.java
@@ -3,7 +3,7 @@ import by.academy.entity.enums.Role;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Data;
@@ -11,13 +11,14 @@ import lombok.Data;
 @Data
 @Builder
 public class UserFullDataDTO {
-    @NotEmpty(message = "Last name cannot be empty")
     @Size(min = 2, max = 40, message = "The length of the string must be between 2 and 40")
+    @Pattern(regexp = "[A-Za-zÀ-ÖØ-Ý]+", message = "Allowed only latin letters!")
     private String lastName;
-    @NotEmpty(message = "First name cannot be empty")
     @Size(min = 2, max = 20, message = "The length of the string must be between 2 and 20")
+    @Pattern(regexp = "[A-Za-zÀ-ÖØ-Ý]+", message = "Allowed only latin letters!")
     private String firstName;
     @Size(max = 40, message = "The string cannot be larger than 40")
+    @Pattern(regexp = "[A-Za-zÀ-ÖØ-Ý]*", message = "Allowed only latin letters!")
     private String middleName;
     @Size(min = 4, max = 50, message = "The length of the string must be between 4 and 50")
     @Email(message = "Email is not valid", regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}$")
